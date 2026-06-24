8006800ac347a2ad1f06e02c77a18f51fc16881d
Juan Pablo <juanpismata11@gmail.com>
2026-06-25 01:50:16 +0200

    arreglo de variables hardcodeadas en templates incidentapi

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,17 +4,17 @@ import * as env from 'env-var';
 dotenv.config();
 
 export const envs = {
-    MAPBOX_TOKEN: env.get("MAPBOX_TOKEN").required().asString(),
-    MAILER_EMAIL: env.get("MAILER_EMAIL").required().asString(),
-    MAILER_PASSWORD: env.get("MAILER_PASSWORD").required().asString(),
-    MAILER_SERVICE: env.get("MAILER_SERVICE").required().asString(),
+    MAPBOX_TOKEN: env.get("MAPBOX_TOKEN").default("pk_dummy").asString(),
+    MAILER_EMAIL: env.get("MAILER_EMAIL").default("noreply@app.com").asString(),
+    MAILER_PASSWORD: env.get("MAILER_PASSWORD").default("dummy").asString(),
+    MAILER_SERVICE: env.get("MAILER_SERVICE").default("gmail").asString(),
     DB_PASSWORD: env.get("DB_PASSWORD").required().asString(),
     DB_NAME: env.get("DB_NAME").required().asString(),
     DB_PORT: env.get("DB_PORT").required().asPortNumber(),
     DB_HOST: env.get("DB_HOST").required().asString(),
     DB_USER: env.get("DB_USER").required().asString(),
     PORT: env.get("PORT").default("3000").asPortNumber(),
-    APPINSIGHTS_CONNECTION_STRING: env.get("APPINSIGHTS_CONNECTION_STRING").required().asString(),
+    APPINSIGHTS_CONNECTION_STRING: env.get("APPINSIGHTS_CONNECTION_STRING").default("").asString(),
     REDIS_HOST: env.get("REDIS_HOST").required().asString(),
     REDIS_PORT: env.get("REDIS_PORT").required().asPortNumber()
 };
